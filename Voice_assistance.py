801ada134150b8d81a0068fe5f355263f9485fe8
Ajay1998-stack-oss <kushwahaajaysingh5@gmail.com>
2021-07-12 18:05:55 +0530

    Added function Search time, music in youtube , Info on wikipedia

--- Voice_assistance.py
+++ Voice_assistance.py
@@ -1,22 +1,54 @@
 import speech_recognition as sr
 import pyttsx3
-
+import pywhatkit
+import datetime
+import wikipedia
 
 listener = sr.Recognizer()
 engine = pyttsx3.init()
 voices = engine.getProperty('voices')
 engine.setProperty('voice',voices[1].id)
+
+
 def talk(text):
-    engine.say('Hello, Ajaaay I am your Voice recorgenization system Alexa.. ')
-    engine.say('What can i do for you')
+    engine.say(text)
     engine.runAndWait()
-try:
-    with sr.Microphone() as source:
-        print("Listening...")
-        voice = listener.listen(source)
-        command = listener.recognize_google(voice)
-        command = command.lower()
-        if 'alexa' in command:
-            print(command)
-except:
-    pass
+
+def take_command():
+    try:
+        with sr.Microphone() as source:
+            print("Listening...")
+
+            voice = listener.listen(source)
+            command = listener.recognize_google(voice)
+            command = command.lower()
+            if 'alexa' in command:
+                command = command.replace('alexa','')
+                print(command)
+    except:
+        pass
+    return command
+
+def run_alexa():
+    command = take_command()
+    print(command)
+    if 'play' in command:
+        song = command.replace('play','') 
+        talk('playing' + song)
+        pywhatkit.playonyt(song )
+    elif 'time' in command:
+        time = datetime.datetime.now().strftime('%I:%M %p')
+        talk('current time is ' + time)
+    elif 'who is ' in command:
+        person = command
+        info = wikipedia.summary(person,1)
+        print(info)
+        talk(info) 
+    elif 'Thankyou' in command:
+        talk("Its my pleassure.. thank you")
+    else:
+        talk("You are not audible properly")
+    
+while True:
+    run_alexa()
+
